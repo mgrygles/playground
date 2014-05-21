09f1aac3b18d5d61a73cf8626f7b0014c872e9de
mgrygles <mgrygles@users.noreply.github.com>
2014-05-21 17:07:18 -0500

    TestMappings.java

---
+++ TestNv/TestMappings.java
@@ -0,0 +1,47 @@
+public class TestMappings {
+
+    private String iso;
+    private String name;
+    private String symbol;
+    private SubUnit subunit;
+    private Links links;
+
+
+    public void setIso(String iso) {
+        this.iso = iso;
+    }
+    public String getIso(){
+        return iso;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    public String getSymbol(){
+        return symbol;
+    }
+
+    public void setSubUnit(SubUnit subunit) {
+        this.subunit = subunit;
+    }
+    public SubUnit getSubUnit(){
+        return subunit;
+    }
+
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+    public Links getLinks() {
+        return links;
+    }
+
+}
+    
